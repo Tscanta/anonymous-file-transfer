ada55d5c4449c976454785211bb58d1871a7e694
mewsickx <tshedupghissing805@gmail.com>
2026-09-18 17:11:52 +0530

    Created the first file drop

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File, HTTPException
 import secrets
 import string
+import os
+import shutil
 
 app = FastAPI(title="Anonymous File Transfer")
+
+
+UPLOAD_DIR = "uploads"
+
+os.makedirs(UPLOAD_DIR, exist_ok=True)
 
 
 def generate_drop_id(length: int = 8) -> str:
@@ -23,4 +30,26 @@ def create_drop():
 
     return {
         "drop_id": drop_id
+    }
+
+
+@app.post("/drops/{drop_id}/files")
+def upload_file(
+    drop_id: str,
+    file: UploadFile = File(...)
+):
+    filename = os.path.basename(file.filename)
+
+    drop_folder = os.path.join(UPLOAD_DIR, drop_id)
+    os.makedirs(drop_folder, exist_ok=True)
+
+    file_path = os.path.join(drop_folder, filename)
+
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    return {
+        "message": "File uploaded successfully",
+        "drop_id": drop_id,
+        "filename": filename
     }
